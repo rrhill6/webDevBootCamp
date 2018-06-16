83e8e624ee33e3caee8a85a59222c7f16f8d78d0
Rochell Hill <rrhill6@yahoo.com>
2018-06-16 18:06:31 -0500

    Updated with changes
    added: 1 question & answer, 1GIF & .animate

--- project-2/project2.js
+++ project-2/project2.js
@@ -1,5 +1,9 @@
 // adds a click event to the questions in the FAQs
 $(".question").click(function() {
-  $(this).next().slideToggle("fast"); // toggles the answer down & hides it again
+  $(this).next().slideToggle().animate({
+    width: "100%"}, 1000 ).animate({
+    fontSize: "1.2em",
+    borderWidth: "10px"
+  }); // toggles the answer down & hides it again
   $(this).children().toggleClass("collapse"); // toggles the arrows to point 'up' & 'down'
 })
