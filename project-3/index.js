6005aeb35bec44352be31a9434925c5b97800f19
Rochell Hill <rrhill6@yahoo.com>
2018-07-03 15:36:54 -0500

    Add files via upload
    Added an 'Easter egg' & animation on delete

--- project-3/index.js
+++ project-3/index.js
@@ -7,16 +7,16 @@ $(document).ready(function () {
     $(".add-items").submit(function (event){
         event.preventDefault();
     // Check input & post item from input in the UL of the HTML
-
-
-    // Append an 'easter egg'...nest an if statement inside the other if statement to
-    // add a gif when a certain phrase is added
         var item = $("#todo-list-item").val();
-        if (item) {
+        if (item) {      
           $("#list-items").append("<li><input class = 'checkbox' type = 'checkbox' />" + item + "<a class='remove'>x</a><hr></li>");
           // Save item in local storage
           localStorage.setItem("listItems", $("#list-items").html());
           $("#todo-list-item").val("");
+          // 'easter egg'... Forrest Gump running gif shows up when user inputs "run"
+          if (item == "run"){
+            $(".running").css("display", "block");
+          }
         }
     });
     
@@ -31,19 +31,12 @@ $(document).ready(function () {
         $(this).parent().toggleClass("completed"); // puts check mark in the box and lines through text
         localStorage.setItem("listItems", $("#list-items").html());
     });
-    // Be able to delete item from list
-
-    // add an animation so something happens before item is removed
-    // go to 'effects' in the documentation for different effects...
-    // $(this).parent().fadeOut("slow", function(){
-    //   $(this).parent.remove(); 
-    //  });
-
+    // Be able to delete item from list (animated slide up when item deleted)
     $(document).on("click", ".remove", function() {
-        $(this).parent().remove();  //removes the list item (li is the parent)
+        $(this).parent().slideUp("slow", function() {
         localStorage.setItem("listItems", $("#list-items").html());
+        });
     });
-
 });
     
     // Use (document) as a selector to check the box/strike through text
